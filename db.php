b1b671a7823179be718021ef0ad4fee90fc8bbac
lulu1417 <boa1417@gmail.com>
2020-03-17 22:40:43 +0800

    add db.php into  autolaod files

--- db.php
+++ db.php
@@ -1,1 +1,26 @@
 <?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+require 'env.php';
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => $config['driver'],
+    'host'      => $config['host'],
+    'database'  => $config['database'],
+    'username'  => $config['username'],
+    'password'  => $config['password'],
+    'charset'   => $config['charset'],
+    'collation' => $config['collation'],
+    'prefix'    => $config['prefix'],
+]);
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
